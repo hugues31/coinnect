f1143d011b77893466fb46aadef6e322fc1b09e9
Hugues Gaillard <hugues.gaillard@me.com>
2017-04-10 22:33:30 +0200

    Delete tests related to non-yet established types (orderbook, history). Fix cfg_attr.

--- tests/coinnect.rs
+++ tests/coinnect.rs
@@ -42,29 +42,9 @@ mod coinnect_tests {
         assert!( ticker.unwrap().timestamp != 0 );
     }
 
-    #[test]
-    fn coinnect_should_return_an_order_book_from_bitstamp() {
-        let mut api = Coinnect::new(Exchange::Bitstamp, "bs_api_key", "bs_api_secret", Some("bs_cust_id"));
-        let order_book = api.return_order_book(Pair::BTC_USD);
-        assert!( order_book.is_some() );
-    }
-    #[test]
-    fn order_book_should_have_bids() {
-        let mut api = Coinnect::new(Exchange::Bitstamp, "bs_api_key", "bs_api_secret", Some("bs_cust_id"));
-        let result = api.return_order_book(Pair::BTC_USD);
-        assert!( result.unwrap().contains_key("bids") );
-    }
-
-    #[test]
-    fn should_return_the_trade_history_for_btc_usd_from_bitstamp() {
-        let mut api = Coinnect::new(Exchange::Bitstamp, "bs_api_key", "bs_api_secret", Some("bs_cust_id"));
-        let result = api.return_trade_history(Pair::BTC_USD);
-
-        assert_eq!( result.is_some(), false );
-    }
-
     // IMPORTANT: Real keys are needed in order to retrieve the balance
     #[test]
+    #[cfg_attr(not(feature = "bitstamp_private_tests"), ignore)]
     fn balance_should_have_usd_btc_fee() {
         use std::path::PathBuf;
         let path = PathBuf::from("./keys_real.json");
