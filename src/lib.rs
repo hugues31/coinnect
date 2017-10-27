3d5bc2981acaf025ea6fa586506d08bd7bf3e734
Hugues Gaillard <hugues.gaillard@me.com>
2017-10-27 15:14:57 +0200

    Fix the clippy lint warning

--- src/lib.rs
+++ src/lib.rs
@@ -19,13 +19,15 @@
 //! can't afford to loose. This is a personal project, I can not be held responsible for
 //! the library malfunction, which can lead to a loss of money.
 
+// error_chain can make a lot of recursions.
+#![recursion_limit="128"]
+
+// Allow lint customization.
+#![allow(unknown_lints)]
+
 // Move all the clippy warning in deny.
 #![deny(clippy)]
 
-// error_chain can make a lot of recursions.
-#![recursion_limit="128"]
-// Allow lint customization.
-#![allow(unknown_lints)]
 // Avoid warning for the Crypto-currency about quotes.
 #![allow(doc_markdown)]
 
